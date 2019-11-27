6b3e3988f8b34ffc4606a50009c21dd49bf3779c
Bosiipo <olabosipo07@gmail.com>
2019-11-28 00:10:19 +0100

    deleted build folder and modified package.json

--- build/config/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var Sequelize = require('sequelize'); // Option 1: Passing parameters separately
-
-
-module.exports = new Sequelize('teamwork', 'postgres', 'Milky5way$', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
